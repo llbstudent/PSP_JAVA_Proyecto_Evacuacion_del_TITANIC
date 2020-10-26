43e46ecca0f718c62e9f8fce7c62c7025876789e
LauraStd <llucbue379@iesalmunia.com>
2020-10-26 13:45:24 +0100

    Modificaciones map -ideas

---
+++ src/DAO_DataModel/DAO_PERSONA2.java
@@ -0,0 +1,7 @@
+package DAO_DataModel;
+
+public class DAO_PERSONA2 {
+	public Treemap<Persona, Persona> mapa = new TreeMao<Persona, Persona()>
+//treemap<persona persona>
+	//sobreEscribir equals
+}
